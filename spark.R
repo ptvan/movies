f58052d1208dd2e7728207a4d9063ab86f5d9d12
Phu T. Van <pvan@fredhutch.org>
2019-10-03 13:50:56 -0700

    first commit of spark.R

---
+++ spark.R
@@ -0,0 +1,24 @@
+library(sparklyr)
+# spark_install(version = "2.4")
+
+## NOTE: Spark 2.x only works on Java 8, so the below assume an OSX environment and
+## that we have run
+## brew cask install homebrew/cask-versions/adoptopenjdk8 
+Sys.setenv(JAVA_HOME = "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home")
+
+conf <- list()
+
+conf$`sparklyr.cores.local` <- 4
+conf$`sparklyr.shell.driver-memory` <- "8G"
+conf$spark.memory.fraction <- 0.6
+
+sc <- spark_connect(master = "local", 
+                    version = "2.4",
+                    config = conf)
+
+# read data directly from disk
+spark_read_csv(sc, "dat","movies.csv")
+
+# alternatively, we can copy objects from the R session into the Spark scope
+# copy_to(sc, iris, "iris")
+
