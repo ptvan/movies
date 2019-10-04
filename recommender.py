a16b189782df113efae03e49edd7385bde52a6b4
Phu T. Van <pvan@fredhutch.org>
2019-10-04 10:33:39 -0700

    updated recommender.py

--- recommender.py
+++ recommender.py
@@ -15,8 +15,15 @@ with open('wp_movies_10k.ndjson') as fin:
 link_counts = Counter()
 for movie in movies:
     link_counts.update(movie[2])
-link_counts.most_common(10)
 
+top_links = [link for link, c in link_counts.items() if c >= 3]
+link_to_idx = {link: idx for idx, link in enumerate(top_links)}
+movie_to_idx = {movie[0]: idx for idx, movie in enumerate(movies)}
+pairs = []
+for movie in movies:
+    pairs.extend((link_to_idx[link], movie_to_idx[movie[0]]) for link in movie[2] if link in link_to_idx)
+pairs_set = set(pairs)
+len(pairs), len(top_links), len(movie_to_idx)
 
 
 
