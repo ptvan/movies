4bfde950d2b8e9d0443a32b30377d42ada6ab59d
ptvan <pvan@fhcrc.org>
2019-10-24 11:05:47 -0700

    updated spark.py

--- spark.py
+++ spark.py
@@ -1,6 +1,7 @@
 import pyspark
 from pyspark import SparkContext
 from pyspark.mllib.recommendation import ALS
+import os
 import math
 
 # remember to add `export PYSPARK_PYTHON=/path/to/python3/` to ~/.bash_profile
@@ -11,7 +12,11 @@ import math
 
 sc = SparkContext("local", "Movie Recommender")
 my_ratings = sc.textFile("my_ratings.csv")
-all_ratings = sc.textFile("movielens-20m/ratings.csv")
+small_ratings_raw = sc.textFile(os.path.join('movielens-100k', 'ratings.csv'))
+small_ratings_raw_header = small_ratings_raw.take(1)[0]
+small_ratings = small_ratings_raw.filter(lambda line: line!=small_ratings_raw_header)\
+    .map(lambda line: line.split(",")).map(lambda tokens: (tokens[0], tokens[1], tokens[2])).cache()
+
 
 seed = 5
 iterations = 10
@@ -25,7 +30,41 @@ min_error = float('inf')
 best_rank = -1
 best_iteration = -1
 
-training, test = all_ratings.randomSplit([7, 3], seed=0)
+training_RDD, validation_RDD, test_RDD = small_ratings.randomSplit([6, 2, 2], seed=0)
+validation_for_predict_RDD = validation_RDD.map(lambda x: (x[0], x[1]))
+test_for_predict_RDD = test_RDD.map(lambda x: (x[0], x[1]))
 
-model = ALS.train(training, best_rank, seed=seed,
-                  iterations=iterations, lambda_=regularization_parameter)
+for rank in ranks:
+    model = ALS.train(training_RDD, rank, seed=seed, iterations=iterations,
+                      lambda_=regularization_parameter)
+    predictions = model.predictAll(validation_for_predict_RDD).map(lambda r: ((r[0], r[1]), r[2]))
+    rates_and_preds = validation_RDD.map(lambda r: ((int(r[0]), int(r[1])), float(r[2]))).join(predictions)
+    error = math.sqrt(rates_and_preds.map(lambda r: (r[1][0] - r[1][1])**2).mean())
+    errors[err] = error
+    err += 1
+   # print 'For rank %s the RMSE is %s' % (rank, error)
+    if error < min_error:
+        min_error = error
+        best_rank = rank
+
+# print 'The best model was trained with rank %s' % best_rank
+
+predictions.take(3)
+
+model = ALS.train(training_RDD, best_rank, seed=seed, iterations=iterations,
+                  lambda_=regularization_parameter)
+predictions = model.predictAll(test_for_predict_RDD).map(lambda r: ((r[0], r[1]), r[2]))
+rates_and_preds = test_RDD.map(lambda r: ((int(r[0]), int(r[1])), float(r[2]))).join(predictions)
+error = math.sqrt(rates_and_preds.map(lambda r: (r[1][0] - r[1][1]) ** 2).mean())
+
+
+complete_ratings_raw_data = sc.textFile(os.path.join('movielens-20m', 'ratings.csv'))
+complete_ratings_raw_data_header = complete_ratings_raw_data.take(1)[0]
+
+complete_ratings_data = complete_ratings_raw_data.filter(lambda line: line!=complete_ratings_raw_data_header)\
+    .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),int(tokens[1]),float(tokens[2]))).cache()
+
+training_RDD, test_RDD = complete_ratings_data.randomSplit([7, 3], seed=0L)
+
+complete_model = ALS.train(training_RDD, best_rank, seed=seed,
+                           iterations=iterations, lambda_=regularization_parameter)
