e4dc45b91304c1a03a5d202238e01e4f0f719a71
Phu T. Van <pvan@fredhutch.org>
2019-10-28 13:42:01 -0700

    updated fetch_movie_metadata.py

--- fetch_movie_metadata.py
+++ fetch_movie_metadata.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import requests
-import json
 
 # read in the OMDBAPI key
 txt = pd.read_table("omdbapi.key")
@@ -9,3 +8,5 @@ key = list(txt.columns)[0]
 # fetch movie metadata
 params = {'apikey':key}
 r = requests.get("http://www.omdbapi.com/?i=tt3896198", params=params)
+synopsis = r.json().get("Plot")
+
